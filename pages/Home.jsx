b9b7d80f22bb4666ffe1623588667c9376da6ea2
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-10 14:07:01 -0400

    add action to form el and create formAction func

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 export function Home(){
+    function formAction(formData){
+        console.log(formData.get("name"))
+    }
+    
     return (
     <React.Fragment>
         <h1>Current user:</h1>
 
-        <form>
+        <form action={formAction}>
             <input type="text" name="name" required/>
             <button type="submit">Update</button>
         </form>

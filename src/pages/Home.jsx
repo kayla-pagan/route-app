4c55e3f68d200bb40eb71ce2540a85471bf22a15
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-10 14:34:36 -0400

    create state to store user name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { updateNameInDB } from "../api";
 
 export function Home(){
+    const [name, setName] = useState(() => JSON.parse(localStorage.getItem("name")) || "Anonymous user")
+
+    async function formAction(formData){
+        try {
+            const newName = await updateNameInDB(formData.get("name"))
+            setName(newName)
+        }
+        catch {
+            console.log(error.message)
+        }
+        
+    }
+    
     return (
     <React.Fragment>
-        <h1>Home</h1>
+        <h1>Current user: <span>{name}</span></h1>
+
+        <form action={formAction}>
+            <input type="text" name="name" required/>
+            <button type="submit">Update</button>
+        </form>
     </React.Fragment>
     )
 }

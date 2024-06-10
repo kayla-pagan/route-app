62c3f5aa96461d697946b6b6f57854722c0656ec
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-10 13:02:08 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+export function App() {
+
+    return (
+        <Home />
+    )
+}

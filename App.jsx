4ca90e9993bc7cf0b150d212ce38a9b76897a408
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-10 13:19:52 -0400

    set up browserrouter and routes

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+export function App() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    )
+}

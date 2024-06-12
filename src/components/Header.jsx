0a0b37541ce66854d266a514b1d964b468620e7a
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-11 21:29:58 -0400

    Create Header component. Add link to home page.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export function Header(){
+    return (
+        <header>
+            <Link to="/">HOME</Link>
+
+            <nav></nav>
+        </header>
+    )
+}

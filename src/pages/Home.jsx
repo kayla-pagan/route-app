a4a3f82b293de1f6f6f655cab15c1a2948708591
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-14 16:04:06 -0400

    add edit icon to form from hero icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ export function Home(){
     const [state, actionFunction, isPending] = useActionState(
         formAction, 
         {
-            name: JSON.parse(localStorage.getItem("name")),
+            name: JSON.parse(localStorage.getItem("name")) || "Anonymous user",
             error: null
-        } || "Anonymous user"
+        } 
     )
 
     const [optimisticName, setOptimisticName] = useOptimistic(state.name)
@@ -28,7 +28,14 @@ export function Home(){
     <React.Fragment>
         <Header />
         <h1>Current user: <span>{optimisticName}</span></h1>
-        {/* add edit icon */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" 
+            />
+        </svg>
+
         {/* when icon is pressed, then display the form below */}
         <form action={actionFunction}>
             <input type="text" name="name" placeholder="enter new username" required/>

7822727ec7d0452420523750698fc8fa37c930c9
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-06-25 21:32:37 -0400

    Add form for username. add username to state.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ export function Home(){
         formAction, 
         {
             name: JSON.parse(localStorage.getItem("name")) || "Anonymous user",
+            username: JSON.parse(localStorage.getItem("username")) || "user1234",
             email: JSON.parse(localStorage.getItem("email")) || "user@example.com",
             error: null
         } 
@@ -46,6 +47,22 @@ export function Home(){
         <Header />
         
         <h1>Current user: <span>{optimisticValue.name}</span></h1>
+        <svg onClick={() => toggleDisplay("name-form")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" 
+            />
+        </svg>
+        {visibleFormID === "name-form" && (
+                <form action={actionFunction} id="name-form">
+                    <input type="text" name="name" placeholder="enter new name" required/>
+                    <button type="submit">Update</button>
+                    <button type="button" onClick={() => setVisibleFormID(null)}>Cancel</button>
+                </form>
+            )}
+
+        <h1>Current user: <span>{optimisticValue.name}</span></h1>
         <svg onClick={() => toggleDisplay("username-form")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path 
                 strokeLinecap="round" 
@@ -55,11 +72,11 @@ export function Home(){
         </svg>
         {visibleFormID === "username-form" && (
                 <form action={actionFunction} id="username-form">
-                    <input type="text" name="name" placeholder="enter new username" required/>
+                    <input type="text" name="username" placeholder="enter new username" required/>
                     <button type="submit">Update</button>
                     <button type="button" onClick={() => setVisibleFormID(null)}>Cancel</button>
                 </form>
-            )}
+            )}    
 
 
         <h1>Current email: <span>{optimisticValue.email}</span></h1>
